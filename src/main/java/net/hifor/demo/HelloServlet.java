c6a4b443d8f3a9607b8d6693e7afcb0e02110141
ikin <ikin@hifor.net>
2023-10-25 20:39:07 +0800

    init

---
+++ src/main/java/net/hifor/demo/HelloServlet.java
@@ -0,0 +1,25 @@
+package net.hifor.demo;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello Servlet!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>" + message + "</h2>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
